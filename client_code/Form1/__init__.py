e8bd48706f393318af80d4636f89452cad44501d
kjell.e.andersen@gmail.com <kjell.e.andersen@gmail.com>
2025-02-05 23:18:51 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +9,21 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Display a popup that says 'You clicked the button'
+    #alert("You clicked the button")
+    name = self.name_box.text
+    email = self.email_box.text
+    feedback = self.feedback_box.text
+    anvil.server.call('send_feedback', name, email, feedback)
+    # Show a popup that says 'Feedback submitted!'
+    Notification("Feedback submitted!").show()
+    self.clear_inputs()
+    
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,29 @@
+import anvil.email
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.callable
+def send_feedback(name, email, feedback):
+  # Send yourself an email each time feedback is submitted
+  anvil.email.send(to="kjell.e.andersen@gmail.com", # Change this to your email address and remove the #!
+                   subject=f"Feedback from {name}",
+                   text=f"""
+                   
+  A new person has filled out the feedback form!
+
+  Name: {name}
+  Email address: {email}
+  Feedback:
+  {feedback}
+  """)
