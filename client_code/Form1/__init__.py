b3631d33f466747e31ed3f2530102c940af7cc15
kjell.e.andersen@gmail.com <kjell.e.andersen@gmail.com>
2025-02-08 01:57:56 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,18 +15,15 @@ class Form1(Form1Template):
     # Display a popup that says 'You clicked the button'
     #alert("You clicked the button")
     name = self.name_box.text
-    email = self.email_box.text
-    feedback = self.feedback_box.text
-    #anvil.server.call('send_feedback', name, email, feedback)
     # Show a popup that says 'Feedback submitted!'
-    Notification("Feedback submitted!").show()
+    Notification("Job(s) started !").show()
     self.clear_inputs()
     
   def clear_inputs(self):
     # Clear our three text boxes
-    self.name_box.text = ""
-    self.email_box.text = ""
-    self.feedback_box.text = ""
+    #self.name_box.text = ""
+    #self.email_box.text = ""
+    #self.feedback_box.text = ""
 
   def unittest_click(self, **event_args):
     """This method is called when the link is clicked"""
