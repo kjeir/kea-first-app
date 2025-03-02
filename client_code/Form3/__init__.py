8b88de0cc28d08f6e78fcb48cde9c71c445971e4
kjell.e.andersen@gmail.com <kjell.e.andersen@gmail.com>
2025-03-02 19:27:42 +0000

    Edited Form3

--- client_code/Form3/__init__.py
+++ client_code/Form3/__init__.py
@@ -16,3 +16,7 @@ class Form3(Form3Template):
   def form_show(self, **event_args):
     """This method is called when the form is shown on the page"""
     self.unit_test_link.role = 'selected'
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('start_tests')
