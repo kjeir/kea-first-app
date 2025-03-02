0ff2218db2cddbd4a951d60c26203c119b14ed6f
Kjell Eirik Andersen <kjell.andersen@silabs.com>
2025-03-02 18:52:36 +0000

    Windows adaptions

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -10,7 +10,12 @@ import subprocess
 #
 @anvil.server.callable
 def start_jobs():
-  command = ["/usr/bin/mate-terminal", "-e", "/home/kean/doit.sh /home/kean/tmp ls -m"]
+  #command = ["/usr/bin/mate-terminal", "-e", "/home/kean/doit.sh /home/kean/tmp ls -m"]
+  #command = ["c:/cygwin64/bin/mintty.exe", "--exec", "c:/cygwin64/bin/bash", "-lic", "c:/tmp/Raptor/doit.sh c:/tmp/Raptor/tmp ls -m"]
+
+  command = ["c:/cygwin64/bin/mintty.exe", "--exec", "c:/cygwin64/bin/bash", "-lic", "c:/tmp/Raptor/doit.sh c:/work/gsdk/extension/se_firmware pipenv run make-fw --no_docker --compiler gcc rainier -j -B debug --development --fpga"]
   subprocess.Popen(command, start_new_session=True)
+
+  command = ["c:/cygwin64/bin/mintty.exe", "--exec", "c:/cygwin64/bin/bash", "-lic", "c:/tmp/Raptor/doit.sh c:/work/gsdk/extension/se_firmware pipenv run make-rom-dev RAINIER --no_docker --compiler gcc release -j -B"]
   subprocess.Popen(command, start_new_session=True)
   
